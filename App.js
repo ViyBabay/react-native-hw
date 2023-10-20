9be8cdad457b743a56a799a3a2025da5bf7378a0
ViyBabay <pikush.oleksandr@gmail.com>
2023-10-20 14:02:07 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,33 @@
+import "react-native-gesture-handler";
+import { StatusBar, Text } from "react-native";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { useFonts } from "expo-font";
+
+import { persistor, store } from "./redux/store";
+import { AppNavigation } from "./components/AppNavigation";
+
+export default function App() {
+  const [fontsLoaded, error] = useFonts({
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+  });
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
+  return (
+    <Provider store={store}>
+      <PersistGate loading={<Text>Loading...</Text>} persistor={persistor}>
+        <StatusBar
+          animated={true}
+          barStyle="dark-content"
+          backgroundColor="#fff"
+        />
+        <AppNavigation />
+      </PersistGate>
+    </Provider>
+  );
+}
